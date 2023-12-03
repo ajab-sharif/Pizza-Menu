4e6730b0de884d32d4539eb444a90a15c9f990c6
sharif <sharif@ajab.sharifulislam.com>
2023-12-04 01:25:21 +0300

    Error handling error element Fetching orders

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable no-unused-vars */
-const BASE_URL = "https://react-fast-pizza-api.onrender.com/api";
 import { useLoaderData } from "react-router-dom";
 import { getMenu } from "./../../services/apiRestaurant";
 import MenuItem from "./MenuItem";
