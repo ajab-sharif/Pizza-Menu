248ce0f6d70ab8f2ed72b43f928988383acf4f27
sharif <sharif@ajab.sharifulislam.com>
2023-12-04 15:07:14 +0300

    tailwind css setup

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div>
-      <h1>
+      <h1 className="text-center  text-xl font-bold  text-green-500 underline">
         The best pizza.
         <br />
         Straight out of the oven, straight to you.
